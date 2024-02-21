37aa7764d4c8fed2b11e64bf94151b8cd821d059
BetaPeche <lolopellegrino@gmail.com>
2024-02-21 14:26:38 +0100

    Restructuring

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,16 @@
-import ErrorContent from "../components/ErrorContent"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
+import { NavLink } from "react-router-dom"
 
 const Error = () => {
     return (
         <>
             <Header />
-            <ErrorContent />
+            <main className="error-content">
+                <h1>404</h1>
+                <h2>Oups! La page que vous demandez n'existe pas.</h2>
+                <NavLink to="/">Retourner sur la page d'accueil</NavLink>
+            </main>
             <Footer />
         </>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import Banner from "../components/Banner"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
-import HomeContent from "../components/HomeContent"
+import logement from "../assets/logements.json"
+import Card from "../components/Card"
 
 const background =
     "linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)),url(../../../assets/img/background.png) center/cover"
@@ -12,7 +13,11 @@ const Home = () => {
         <>
             <Header />
             <Banner text={text} background={background} />
-            <HomeContent />
+            <main className="home-content">
+                {logement.map((logement, index) => (
+                    <Card key={index} logement={logement} />
+                ))}
+            </main>
             <Footer />
         </>
     )
