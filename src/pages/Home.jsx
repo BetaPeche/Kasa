18148615a81656c8357d35adc7f534a8a8c6da40
BetaPeche <lolopellegrino@gmail.com>
2024-02-21 15:50:23 +0100

    fetch function for get data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,35 @@
 import Banner from "../components/Banner"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
-import logement from "../assets/logements.json"
 import Card from "../components/Card"
-
-const background =
-    "linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)),url(../../../assets/img/background.png) center/cover"
-const text = "Chez vous, partout et ailleurs"
+import { useEffect, useState } from "react"
 
 const Home = () => {
+    const [data, setData] = useState([])
+    const background =
+        "linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)),url(../../../assets/img/background.png) center/cover"
+    const text = "Chez vous, partout et ailleurs"
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/src/assets/logements.json")
+                const surveyData = await response.json()
+                setData(surveyData)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchData()
+    }, [])
+
     return (
         <>
             <Header />
             <Banner text={text} background={background} />
             <main className="home-content">
-                {logement.map((logement, index) => (
-                    <Card key={index} logement={logement} />
+                {data.map((data, index) => (
+                    <Card key={index} logement={data} />
                 ))}
             </main>
             <Footer />
