9f4a337938ab2d3ca655a4cd342bb9283190fd4f
BetaPeche <lolopellegrino@gmail.com>
2024-02-27 09:10:49 +0100

    Inverse li and link balises

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,18 +4,22 @@ const Navigation = () => {
     return (
         <nav>
             <ul className="navigation">
-                <NavLink
-                    to="/"
-                    className={(nav) => (nav.isActive ? "active-link" : "")}
-                >
-                    <li>Accueil</li>
-                </NavLink>
-                <NavLink
-                    to="/about"
-                    className={(nav) => (nav.isActive ? "active-link" : "")}
-                >
-                    <li>A Propos</li>
-                </NavLink>
+                <li>
+                    <NavLink
+                        to="/"
+                        className={(nav) => (nav.isActive ? "active-link" : "")}
+                    >
+                        Accueil
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/about"
+                        className={(nav) => (nav.isActive ? "active-link" : "")}
+                    >
+                        A Propos
+                    </NavLink>
+                </li>
             </ul>
         </nav>
     )
