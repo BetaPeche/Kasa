3df45d011389426d626183f1c18657ce4c6cf8d2
BetaPeche <lolopellegrino@gmail.com>
2024-03-07 11:30:56 +0100

    Remove style in Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,13 +4,9 @@ import { Link } from "react-router-dom"
 const Card = ({ logement }) => {
     return (
         <Link to={`/housing/${logement.id}`}>
-            <article
-                className="card"
-                style={{
-                    background: `linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, rgba(10, 10, 10, 0.00) 42%, rgba(4, 4, 4, 0.20) 99.99%, rgba(0, 0, 0, 0.50) 100%),url(${logement.cover}) center/cover`,
-                }}
-            >
-                {logement.title}
+            <article className="card">
+                <img src={logement.cover} alt="" />
+                <h2>{logement.title}</h2>
             </article>
         </Link>
     )
